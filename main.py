793e897d324dae65ce9d86eb64ae495e953cb503
Tanya Gupta <gupta.tanya@gmail.com>
2019-10-26 17:28:56 -0400

    feat: fixed code

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import sys
+import collections
+
+def writefile(filename,review):
+    myfile = open  (filename, 'w')
+    myfile.writelines (review)
+    myfile.close ()
+    print ("successfully written "+filename)
 
 def fileopen (file_name):
-
     file_content_as_string = " "
-
     try:
         file_handle = open(file_name, "r")
         file_content_as_string = file_content_as_string.join(file_handle.readlines())
@@ -11,27 +16,38 @@ def fileopen (file_name):
         return (file_content_as_string)
     except IndexError:
         print ("No file name to read")
+
 def parse_data(feelings,template):
     feelings_source = feelings.split('#')
     feelings_source.pop(0)
-    #feelings_source = [i.replace('\n','') for i in feelings_source] # use this if you want to get rid of the \n
-    tags = dict(zip(feelings_source[::2], feelings_source[1::2]))
-    template = template.split(" ")
-    #print(template) # ['I', 'chose', '<property_name>', 'because', '<reasons>
-    review=""
-    for item in template:
-        if (not item.startswith("<")):
-            review = review +" "+ item
-            #print(review)
-        else: #if item.startswith("<") and (item.endswith(">.\n") or item.endswith(">.")or item.endswith(">")):
-            item = item.strip('>.')
-            item = item.strip('>')
-            item = item.strip('>.\n')
-            item = item.strip('<')
-            review = review +tags[item]
+    if "property_name" in feelings_source and "dates" in feelings_source:
+        #print(feelings_source)
+        #feelings_source = [i.replace('\n','') for i in feelings_source] # use this if you want to get rid of the \n
+        #tags = dict(zip(feelings_source[::2], feelings_source[1::2]))
+        #tags = collections.defaultdict(lambda : 'Key Not found')
+        tags = dict(zip(feelings_source[::2], feelings_source[1::2]))
+        template = template.split(" ")
+        review=""
+        for item in template:
+            if (not item.startswith("<")):
+                review = review +" "+ item
+                #print(review)
+            else: #if item.startswith("<") and (item.endswith(">.\n") or item.endswith(">.")or item.endswith(">")):
+                item = item.strip('>.')
+                item = item.strip('>')
+                item = item.strip('>.\n')
+                item = item.strip('<')
+                if item in tags:
+                    review = review +tags[item]
+                else:
+                    review = review + ""
 
-    review = review.replace('\r', '').replace('\n', '')
-    print(review)
+        review = review.replace('\r', '').replace('\n', '')
+        filename = (feelings_source[1]).strip().replace(' ','_')
+        filename = filename +".txt"
+        writefile(filename,review)
+    else:
+        print("error")
 
 
 def main():
